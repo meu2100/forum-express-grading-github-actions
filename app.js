4b902d38801d1532863e18b2e5342e7ec43dcfa5
meu2100 <amanda8018@gmail.com>
2025-02-10 22:07:02 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 const routes = require('./routes')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
@@ -21,6 +22,7 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
