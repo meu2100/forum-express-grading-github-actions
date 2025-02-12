7a6cf44e0c0742fc3f90d253701d513e39b939e2
meu2100 <amanda8018@gmail.com>
2025-02-12 20:09:48 +0800

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,6 +1,10 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
+const relativeTime = require('dayjs/plugin/relativeTime') // 增加這裡
+dayjs.extend(relativeTime)
+
 module.exports = {
   currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(), // 增加這裡
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
